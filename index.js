faa1a7c1e39b902c14c61b5ac17129f7e7345045
khoa <khoatran121294@gmail.com>
2017-05-24 17:41:55 +0700

    update code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const PORT = 3000;
-// app.set('superSecret', 'todos_secret'); // secret variable
+app.set('superSecret', 'THIS_IS_SECRET'); // secret variable
 
 app.get('/', function (req, res) {
     res.send('Todo app server');
@@ -20,15 +20,20 @@ const router = express.Router();
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 router.post('/authenticate', function (req, res) {
     const account = req.body.account;
-
-    if ("admin" != account.username || "123456" != account.password) {
+    console.log(account);
+    if ("admin" !== account.username || "123456" !== account.password) {
         res.json({ success: false, message: 'Authentication failed. User not found.' });
     }
     else {
         // if user is found and password is right
         // create a token
+        // not save pass in payload
+        account.password = "PASS_IS_NOT_HERE";
         var token = jwt.sign(account, app.get('superSecret'), {
-            expiresInMinutes: 1440 // expires in 24 hours
+            
+            // expiresInMinutes is was deprecated from docs
+            // expiresInMinutes: 1440 // expires in 24 hours
+            expiresIn : 10
         });
 
         // return the information including token as JSON
@@ -42,9 +47,33 @@ router.post('/authenticate', function (req, res) {
 
 // route middleware to verify a token
 router.use(function (req, res, next) {
-    console.log('oke');
     // check header or url parameters or post parameters for token
-    next();
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'),{ignoreExpiration : true} ,function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;  
+        console.log(req.decoded);  
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+  }
 });
 
 router.get('/', function (req, res) {
